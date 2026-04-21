dc6d4379be9984eefbf567c07630b60174e6409f
hereward99 <nicholas.manthorpe@gmail.com>
2026-04-21 18:26:23 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,16 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+//  server: {
+//    host: "::",
+//    port: 8080,
+// This ensures assets load from /chronicle/ instead of /
+      base: "/chronicle/", 
+      plugins: [react()],
+      resolve: {
+      alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   plugins: [
     react(),
     mode === "development" && componentTagger(),
